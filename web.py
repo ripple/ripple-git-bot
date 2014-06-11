f7957629f9efd5b1cda31394a52037858e46c1eb
evhub <evanjhub@gmail.com>
2014-06-11 12:49:50 -0700

    Fixes some errors with variable names

--- web.py
+++ web.py
@@ -8,19 +8,24 @@ from flask import Flask
 from gitbot import printdebug, main
 from config import params
 
+# Setting Up:
+
 global working
 working = False
 
 app = Flask(__name__)
 
+# Running The Main Function:
+
 @app.route("/")
 def run():
+    global working
     printdebug(params, "Initializing...")
     if working:
         printdebug(params, "    Failed due to concurrent boot.")
     else:
         working = True
-        members, openpulls, merges = main(params)
+        memberlist, openpulls, merges = main(params)
         printdebug(params, "Members: "+repr(memberlist)+"\nPull Requests: "+repr(openpulls)+"\nMerges: "+repr(merges))
         working = False
         return "GitHub pull requests succesfully analyzed. Merged "+str(len(merges))+" pull requests."
