bba48fc991b0eb2f5144a97af5378ab4d5f833b1
evhub <evanjhub@gmail.com>
2014-06-11 12:42:14 -0700

    Attempts to prevent concurrency errors

--- web.py
+++ web.py
@@ -8,11 +8,19 @@ from flask import Flask
 from gitbot import printdebug, main
 from config import params
 
+global working
+working = False
+
 app = Flask(__name__)
 
 @app.route("/")
 def run():
     printdebug(params, "Initializing...")
-    members, openpulls, merges = main(params)
-    printdebug(params, "Members: "+repr(memberlist)+"\nPull Requests: "+repr(openpulls)+"\nMerges: "+repr(merges))
-    return "GitHub pull requests succesfully analyzed. Merged "+str(len(merges))+" pull requests."
+    if working:
+        printdebug(params, "    Failed due to concurrent boot.")
+    else:
+        working = True
+        members, openpulls, merges = main(params)
+        printdebug(params, "Members: "+repr(memberlist)+"\nPull Requests: "+repr(openpulls)+"\nMerges: "+repr(merges))
+        working = False
+        return "GitHub pull requests succesfully analyzed. Merged "+str(len(merges))+" pull requests."
