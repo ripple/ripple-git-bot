19b65da0ec239e9a9c457bba31654afb9b28a8b1
Evan Hubinger <evhub@ripple.com>
2014-06-11 16:59:49 -0700

    Sets config params for testing

--- config.py
+++ config.py
@@ -12,9 +12,9 @@ params = {
     "downs" : [],                                                   # What strings register as downvotes ('')
     "message" : "Verified passes tests by <cibotname>. Verified looks good to <voters>.",        # The message displayed by the bot on merge
     "botname" : "ripplebot",                                        # The name of the ripple bot
-    "orgname" : "ripple",                                            # The name of ripple's github organization
-    "cibotname" : "mtrippled",                                      # The name of the ripple CI bot
-    "hookurl" : "http://ripple-git-bought.herokuapp.com/",          # The url of the server file for hooking into
+    "orgname" : "ripple-git-test",                                  # The name of ripple's github organization
+    "cibotname" : "evhub",                                           # The name of the ripple CI bot
+    "hookurl" : "http://ripple-git-bot.herokuapp.com/",               # The url of the server file for hooking into
     "hookname" : "web",                                              # The new name of the hooks into this file
     "hooknames" : ["web"],                                          # All the names of the hooks into this file
     "hookevents" : [                                                # The different events the hook is triggered on
