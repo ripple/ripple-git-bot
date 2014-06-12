e26930664309096f453658200b8f53789156f521
Evan Hubinger <evhub@ripple.com>
2014-06-11 17:35:45 -0700

    Removes some variable defaults

--- config.py
+++ config.py
@@ -9,18 +9,18 @@ import os
 
 params = {
     "token" : "",                                                   # The login token for the bot (this should usually be passed as an environment variable)
-    "orgname" : "ripple",                                            # The name of ripple's github organization
-    "cibotname" : "mtrippled",                                      # The name of the ripple CI bot
-    "hookurl" : "http://ripple-git-bought.herokuapp.com/",          # The url of the server file for hooking into
+    "orgname" : "",                                                 # The name of ripple's github organization
+    "cibotname" : "",                                               # The name of the ripple CI bot
+    "hookurl" : "",                                                 # The url of the server file for hooking into
     "lgtms" : ["lgtm"],                                             # What strings register as upvotes (no leading or trailing whitespace and all lowercase)
-    "vetoes" : ["veto"],                                             # What strings register as veto votes ('')
+    "vetoes" : ["veto"],                                            # What strings register as veto votes ('')
     "downs" : [],                                                   # What strings register as downvotes ('')
     "message" : "Verified passes tests by <cibotname>. Verified looks good to <voters>.",        # The message displayed by the bot on merge
     "botname" : "ripplebot",                                        # The name of the ripple bot
     "orgname" : "ripple-git-test",                                  # The name of ripple's github organization
-    "cibotname" : "evhub",                                           # The name of the ripple CI bot
-    "hookurl" : "http://ripple-git-bot.herokuapp.com/",               # The url of the server file for hooking into
-    "hookname" : "web",                                              # The new name of the hooks into this file
+    "cibotname" : "evhub",                                          # The name of the ripple CI bot
+    "hookurl" : "http://ripple-git-bot.herokuapp.com/",             # The url of the server file for hooking into
+    "hookname" : "web",                                             # The new name of the hooks into this file
     "hooknames" : ["web"],                                          # All the names of the hooks into this file
     "hookevents" : [                                                # The different events the hook is triggered on
                  "commit_comment",
@@ -32,7 +32,7 @@ params = {
                  ],
     "message" : "Verified passes tests by <cibotname>. Verified looks good to <voters>.",        # The message displayed by the bot on merge
     "lgtms" : ["lgtm"],                                             # What strings register as upvotes (no leading or trailing whitespace and all lowercase)
-    "vetoes" : ["veto"],                                             # What strings register as veto votes ('')
+    "vetoes" : ["veto"],                                            # What strings register as veto votes ('')
     "downs" : [],                                                   # What strings register as downvotes ('')
     "votecount" : 2,                                                # The number of LGTM votes required to merge
     "debug" : True                                                  # Turns on and off verbose debug output
